2234389207abace5aa621195045f36b9296f90e4
maks969 <111703818+maks969@users.noreply.github.com>
2022-09-12 23:47:11 +0300

    задача 1 правки+++

--- task 1/Program.cs
+++ task 1/Program.cs
@@ -1,29 +1,27 @@
 ﻿// программа на вход два числа, выводит большее и меньшее.
-// 5-7.2-10.-9--3.
+// 5-7.2-10
+        
+Console.WriteLine($"введите первое число-");
+            string input1 = Console.ReadLine();
+        
+Console.WriteLine($"введите второе число-");
+            string input2 = Console.ReadLine();
+          
+  int a = int.Parse(input1);
+  int b = int.Parse(input2);
+    
+        int max = a;
+        int min = b;
+        if (min == max)
+        {
+            Console.WriteLine("числа равны!");
+            return;
+        }
+        if (b > max)
+        {
+            max = b;
+            min = a;
+        }
 
-
-
-
-    Console.WriteLine($"введите первое число-");
-  string input1 = Console.ReadLine();
-int a = int.Parse(input1);
-
-    Console.WriteLine($"введите второе число-");
-  string input2 = Console.ReadLine();
-int b = int.Parse(input2);
-
-int max = a;
-int min = b;
-if (min == max)
-{
- Console.WriteLine("числа равны!");
- return;
-}
-  if (b>max)
-  {
-     max = b;
-     min = a;
-  }
-
-Console.Write($"max ={max}");
-Console.Write($"min ={min}");
+        Console.Write($"max ={max}");
+        Console.Write($"min ={min}");
