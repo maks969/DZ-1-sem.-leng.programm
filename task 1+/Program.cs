92663ecee061c4eb27fc2e39fa0171218e566131
maks969 <111703818+maks969@users.noreply.github.com>
2022-09-13 00:32:12 +0300

    задача 1 просто+

---
+++ task 1+/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine($"введите первое число-");
+            string input1 = Console.ReadLine();
+        
+Console.WriteLine($"введите второе число-");
+            string input2 = Console.ReadLine();
+          
+  int a = int.Parse(input1);
+  int b = int.Parse(input2);
+Console.WriteLine($"min = {Math.Min(a, b)}");
+Console.WriteLine($"max = {Math.Max(a, b)}");
