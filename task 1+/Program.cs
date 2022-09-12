d356170f8ff45915dd48acf814046b69238697ed
maks969 <111703818+maks969@users.noreply.github.com>
2022-09-13 00:44:30 +0300

    только положительные числа

--- task 1+/Program.cs
+++ task 1+/Program.cs
@@ -1,4 +1,5 @@
-﻿Console.WriteLine($"введите первое число-");
+﻿// только положительные числа !!!
+Console.WriteLine($"введите первое число-");
             string input1 = Console.ReadLine();
         
 Console.WriteLine($"введите второе число-");
@@ -6,5 +7,8 @@ Console.WriteLine($"введите второе число-");
           
   int a = int.Parse(input1);
   int b = int.Parse(input2);
+
+  if (a == b) Console.WriteLine("числа равны!");return;
+
 Console.WriteLine($"min = {Math.Min(a, b)}");
 Console.WriteLine($"max = {Math.Max(a, b)}");
